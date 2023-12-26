a04297c3fa209e156b6d959b999fa1aff2d9271c
Flavien Afonso <af.flavien@gmail.com>
2023-12-26 11:10:18 +0100

    change in displayRecipes, to handle cases where there is no match

--- scripts/templates/recipe.js
+++ scripts/templates/recipe.js
@@ -2,9 +2,14 @@ function displayRecipes(recipes) {
   const recipeSectionEl = document.querySelector(".recipe-section");
   recipeSectionEl.innerHTML = "";
   const recipeNumEl = document.querySelector(".filters-section__recipes-found");
-  recipeNumEl.textContent = `${recipes.length} Recettes`;
-
-  recipes.forEach((item) => recipeSectionEl.append(item.domCard));
+  recipeNumEl.textContent = `${recipes.length} Recette${recipes.length > 1 ? "s" : ""}`;
+  if (recipes.length === 0) {
+    recipeSectionEl.innerHTML = `Aucune recette ne contient 
+        ‘${document.querySelector(".main-search-bar input").value}’ 
+        vous pouvez chercher « tarte aux pommes », « poisson », etc.`;
+  } else {
+    recipes.forEach((item) => recipeSectionEl.append(item.domCard));
+  }
 }
 
 
